42b1009290b827c4b3177863968fc9730e81573e
Jeff Marks <jeff.t.marks@gmail.com>
2022-07-17 21:17:29 -0400

    commit tree b2bfff9834759ff4b03f2fae5c3b59dbb2a61e32

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random());
+
+let num3 = 13;
+let num4 = 9;
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 3, 20);
